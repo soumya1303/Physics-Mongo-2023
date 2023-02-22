7970b2fab6152aec5fd3044b9dcc9c1f2cfd90b9
soumya1303 <soumya_rbs@yahoo.co.in>
2023-02-22 23:49:52 +0000

    22-02-2023 changed localhost port to 4000 to avoid clash with React apps

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
